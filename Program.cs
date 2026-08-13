0eb6b06d031565c2402c33b6b22ec49edeef094b
Salma Mahmoud <salma24006539@gmail.com>
2026-08-13 23:36:10 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace ConsoleApp1
+{
+    internal class program
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
